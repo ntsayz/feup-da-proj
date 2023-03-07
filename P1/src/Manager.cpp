b92f9bd06171886763bf604bdb46cc426aa20460
Ntsay Zacarias <ntsayyzacarias@gmail.com>
2023-03-07 15:55:09 +0000

    Adding boilerplate code

---
+++ P1/src/Manager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ntsay Zacarias on 07/03/2023.
+//
+
+#include "Manager.h"
---
+++ P1/src/Manager.h
@@ -0,0 +1,14 @@
+//
+// Created by Ntsay Zacarias on 07/03/2023.
+//
+
+#ifndef SRC_MANAGER_H
+#define SRC_MANAGER_H
+
+
+class Manager {
+
+};
+
+
+#endif //SRC_MANAGER_H
